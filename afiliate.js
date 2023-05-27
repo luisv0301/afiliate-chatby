9f145d7dbbf4d28dd63a3aedc8f3c3a5189a02af
Luis Vasquez <luisvbdr0301@gmail.com>
2023-05-27 12:53:21 -0400

    new test to test

--- afiliate.js
+++ afiliate.js
@@ -283,13 +283,23 @@ const getInitialData = () => {
         const pruebaURL =
           "https://snazzy-khapse-28b8e0.netlify.app/afiliate-register";
         const URLoficial = "https://chatby.io/affiliate-sign-up";
-        const url = new URL(pruebaURL);
+
+        let urlTest = new URL(pruebaURL);
+        let params = new URLSearchParams(urlTest.search);
+
+        //Add a second foo parameter.
+        params.append("workspace_id", workspace_id);
+        params.append("workspace_name", workspace_name);
+        params.append("owner_name", owner_name);
+        params.append("owner_email", owner_email);
+
+        /* const url = new URL(pruebaURL);
         url.searchParams.set("workspace_id", workspace_id);
         url.searchParams.set("workspace_name", workspace_name);
         url.searchParams.set("owner_name", owner_name);
         url.searchParams.set("owner_email", owner_email);
-
-        window.location.assign(url);
+*/
+        window.location.assign(urlTest);
       } else {
         const pruebaURL =
           "https://snazzy-khapse-28b8e0.netlify.app/afiliate-register";
